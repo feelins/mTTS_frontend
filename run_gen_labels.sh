39ed6ee26c17a9d04cdf8aa9c06ead7ce7f12f2c
feelins <feipengshao@163.com>
2019-05-07 10:59:49 +0800

    create the run_gen_label shell

---
+++ run_gen_labels.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+work_dir='/home/shaopf/study/mTTS_frontend'
+voice_name=biaobei_test
+data_dir=${work_dir}/data
+voice_path=${data_dir}/${voice_name}
+output_path=${voice_path}/output_align
+textgrid_path=${output_path}/wav
+
+echo ${voice_path}/example_hanlp_txt.txt ${voice_path}/example_hanlp_pos_new.txt ${voice_path}/example_hanlp_pinyin.txt ${textgrid_path}
+python src/mtts.py ${voice_path}/example_hanlp_txt.txt ${voice_path}/example_hanlp_pos_new.txt ${voice_path}/example_hanlp_pinyin.txt ${textgrid_path}
+
